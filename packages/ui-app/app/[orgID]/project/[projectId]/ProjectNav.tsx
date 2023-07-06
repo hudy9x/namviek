4390bd63c47fec9f46c2574a7eb6552de99d04a4
hudy9x <hudy9x@gmail.com>
2023-07-06 16:44:48 +0700

    retab

--- packages/ui-app/app/[orgID]/project/[projectId]/ProjectNav.tsx
+++ packages/ui-app/app/[orgID]/project/[projectId]/ProjectNav.tsx
@@ -9,47 +9,47 @@ import { HiOutlineMenuAlt1 } from 'react-icons/hi';
 import TaskCreate from './TaskCreate';
 
 export default function ProjectNav() {
-	const searchParams = useSearchParams();
-	const { push } = useRouter();
-	const params = useParams();
-	const { selectedProject } = useProjectStore(state => state);
-	const mode = searchParams.get('mode');
+  const searchParams = useSearchParams();
+  const { push } = useRouter();
+  const params = useParams();
+  const { selectedProject } = useProjectStore(state => state);
+  const mode = searchParams.get('mode');
 
-	const [tabs] = useState([
-		{ name: 'Overview', href: '#', icon: HiOutlineUserCircle, current: false },
-		{ name: 'Task', href: '#', icon: HiOutlineMenuAlt1, current: false },
-		{ name: 'Board', href: '#', icon: HiOutlineViewColumns, current: false },
-		{ name: 'Calendar', href: '#', icon: HiOutlineCalendar, current: false },
-		{ name: 'Setting', href: '#', icon: HiOutlineCog6Tooth, current: false }
-	]);
+  const [tabs] = useState([
+    { name: 'Overview', href: '#', icon: HiOutlineUserCircle, current: false },
+    { name: 'Task', href: '#', icon: HiOutlineMenuAlt1, current: false },
+    { name: 'Board', href: '#', icon: HiOutlineViewColumns, current: false },
+    { name: 'Calendar', href: '#', icon: HiOutlineCalendar, current: false },
+    { name: 'Setting', href: '#', icon: HiOutlineCog6Tooth, current: false }
+  ]);
 
-	const onMoveTab = (name: string) => {
-		console.log(params);
-		push(`${params.orgID}/project/${params.projectId}?mode=${name.toLowerCase()}`);
-	};
+  const onMoveTab = (name: string) => {
+    console.log(params);
+    push(`${params.orgID}/project/${params.projectId}?mode=${name.toLowerCase()}`);
+  };
 
-	return (
-		<div className="project-nav">
-			<div className="bg-white border-b border-gray-200">
-				<h2 className="text-xl font-bold px-4 py-2">{selectedProject?.name}</h2>
-				<div className="tab pl-1">
-					{tabs.map((tab, index) => {
-						const Icon = tab.icon;
-						const active = tab.name.toLowerCase() === mode;
-						return (
-							<div onClick={() => onMoveTab(tab.name)} className={`tab-item ${active ? 'active' : ''}`} key={index}>
-								<Icon />
-								<span>{tab.name}</span>
-							</div>
-						);
-					})}
-				</div>
-			</div>
+  return (
+    <div className="project-nav">
+      <div className="bg-white border-b border-gray-200">
+        <h2 className="text-xl font-bold px-4 py-2">{selectedProject?.name}</h2>
+        <div className="tab pl-1">
+          {tabs.map((tab, index) => {
+            const Icon = tab.icon;
+            const active = tab.name.toLowerCase() === mode;
+            return (
+              <div onClick={() => onMoveTab(tab.name)} className={`tab-item ${active ? 'active' : ''}`} key={index}>
+                <Icon />
+                <span>{tab.name}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
 
-			<div className="task bg-indigo-50/50 w-full">
-				<ProjectTabContent />
-			</div>
-			<TaskCreate />
-		</div>
-	);
+      <div className="task bg-indigo-50/50 w-full">
+        <ProjectTabContent />
+      </div>
+      <TaskCreate />
+    </div>
+  );
 }
