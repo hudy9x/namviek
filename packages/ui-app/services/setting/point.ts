019dbc67f6be166d607c1a4e83af4c8dde7727e8
hudy9x <hudy9x@gmail.com>
2023-07-06 21:33:52 +0700

    convert tab to spaces

--- packages/ui-app/services/setting/point.ts
+++ packages/ui-app/services/setting/point.ts
@@ -2,10 +2,10 @@ import { TaskPoint } from '@prisma/client';
 import { httpGet, httpPost } from '../_req';
 
 export const taskPointCreate = (data: Pick<TaskPoint, 'point' | 'projectId'>) => {
-	console.log({ data });
-	return httpPost('/api/setting/point', data);
+  console.log({ data });
+  return httpPost('/api/setting/point', data);
 };
 
 export const taskPointGet = (projectId: string) => {
-	return httpGet('/api/setting/point');
+  return httpGet('/api/setting/point');
 };
