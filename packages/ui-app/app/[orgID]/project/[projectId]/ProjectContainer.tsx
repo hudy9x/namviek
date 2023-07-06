82de1a454247b954d8a0668cad7a2d78d63fedd0
hudy9x <hudy9x@gmail.com>
2023-07-06 16:40:47 +0700

    retab

--- packages/ui-app/app/[orgID]/project/[projectId]/ProjectContainer.tsx
+++ packages/ui-app/app/[orgID]/project/[projectId]/ProjectContainer.tsx
@@ -7,19 +7,19 @@ import { useParams } from 'next/navigation';
 import { useMemberStore } from '../../../../store/member';
 
 export default function ProjectContainer() {
-	const params = useParams();
-	const { addAllMember } = useMemberStore();
+  const params = useParams();
+  const { addAllMember } = useMemberStore();
 
-	useEffect(() => {
-		getProjectMember(params.projectId).then(res => {
-			const { data, status } = res.data;
-			if (status !== 200) {
-				addAllMember([]);
-				return;
-			}
+  useEffect(() => {
+    getProjectMember(params.projectId).then(res => {
+      const { data, status } = res.data;
+      if (status !== 200) {
+        addAllMember([]);
+        return;
+      }
 
-			addAllMember(data);
-		});
-	}, []);
-	return <ProjectNav />;
+      addAllMember(data);
+    });
+  }, []);
+  return <ProjectNav />;
 }
