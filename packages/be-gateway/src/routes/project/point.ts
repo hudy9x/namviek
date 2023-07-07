ffed1711b1f016492d2a75ea32c97ec88d903473
huypl53 <phamlehuy53@gmail.com>
2023-07-07 22:49:13 +0700

    merge `refactor/get-project-settings`, adding no-semi rule

--- packages/be-gateway/src/routes/project/point.ts
+++ packages/be-gateway/src/routes/project/point.ts
@@ -1,20 +1,38 @@
-import { Router } from 'express';
-import { AuthRequest } from '../../types';
-import { mdTaskPointGetByProjectId } from '@shared/models';
+import { Router } from 'express'
+import { AuthRequest } from '../../types'
+import { TaskPoint } from '@prisma/client'
+import { mdTaskPointAddOne, mdTaskPointGetByProjectId } from '@shared/models'
 
-const router = Router();
+const router = Router()
+
+router.post('/project/setting/point', async (req: AuthRequest, res) => {
+  const body = req.body as Pick<TaskPoint, 'point' | 'projectId' | 'icon'>
+  try {
+    const result = await mdTaskPointAddOne({
+      ...body
+    })
+    res.json({
+      status: 200,
+      data: result
+    })
+  } catch (e) {
+    res.json({
+      status: 400,
+      error: e
+    })
+  }
+})
 
 router.get('/project/point/:projectId', async (req: AuthRequest, res) => {
-	const projectId = req.params.projectId;
+  const projectId = req.params.projectId
 
-	mdTaskPointGetByProjectId(projectId)
-		.then(result => {
-			console.log(result);
-			res.json({ status: 200 });
-		})
-		.catch(err => {
-			console.log(err);
-		});
-});
-
-export default router;
+  mdTaskPointGetByProjectId(projectId)
+    .then(result =>
+      res.json({
+        status: 200,
+        data: result
+      })
+    )
+    .catch(err => console.log({ err }))
+})
+export default router
--- packages/shared-models/src/lib/taskPoint.ts
+++ packages/shared-models/src/lib/taskPoint.ts
@@ -1,9 +1,36 @@
-import { taskPointModel } from './_prisma';
+import { TaskPoint } from '@prisma/client'
+import { taskPointModel } from './_prisma'
+
+export const mdTaskPointGetManyByProjectIds = async (projectIds: string[]) => {
+  return taskPointModel.findFirst({
+    where: {
+      id: {
+        in: projectIds
+      }
+    }
+  })
+}
+
+// export const mdTskPointGetOne = async (projectId: string | string[]) => {
+// 	return tskPointModel.findFirst({
+// 		where: {
+// 			id: {
+// 				in: Array.isArray(projectId) ? projectId : [projectId]
+// 			setting/point}
+// 		}
+// 	});
+// };
 
 export const mdTaskPointGetByProjectId = async (projectId: string) => {
   return taskPointModel.findMany({
     where: {
       projectId
     }
-  });
-};
+  })
+}
+
+export const mdTaskPointAddOne = async (data: Pick<TaskPoint, 'point' | 'projectId' | 'icon'>) => {
+  return taskPointModel.create({
+    data: data
+  })
+}
