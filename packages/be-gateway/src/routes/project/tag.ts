dac4060869ddc299970e158461e09437ace32f9e
hudy9x <hudy9x@gmail.com>
2023-07-07 11:17:56 +0700

    do not use tab

--- packages/be-gateway/src/routes/project/tag.ts
+++ packages/be-gateway/src/routes/project/tag.ts
@@ -5,16 +5,16 @@ import { mdTagGetByProjectId } from '@shared/models';
 const router = Router();
 
 router.get('/project/tag/:projectId', async (req: AuthRequest, res) => {
-	const projectId = req.params.projectId;
+  const projectId = req.params.projectId;
 
-	mdTagGetByProjectId(projectId)
-		.then(result => {
-			console.log(result);
-			res.json({ status: 200 });
-		})
-		.catch(err => {
-			console.log(err);
-		});
+  mdTagGetByProjectId(projectId)
+    .then(result => {
+      console.log(result);
+      res.json({ status: 200 });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 export default router;
