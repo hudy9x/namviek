0868097e63869d40281f2169c23f76b97c102321
hudy9x <hudy9x@gmail.com>
2023-08-03 15:30:45 +0700

    update chart column

--- packages/ui-app/app/_features/Dashboard/components/DbCompColumn.tsx
+++ packages/ui-app/app/_features/Dashboard/components/DbCompColumn.tsx
@@ -1,32 +1,10 @@
-import {
-  IDBComponentConfig,
-  dboardQueryColumn,
-  dboardQuerySummary
-} from '@/services/dashboard'
+import { IDBComponentConfig, dboardQueryColumn } from '@/services/dashboard'
 import { useEffect, useState } from 'react'
 import { AiOutlineLock, AiOutlineSync } from 'react-icons/ai'
-import { ICompConfig, IDbCompProps, refactorConfig } from '../type'
+import { IDbCompProps, refactorConfig } from '../type'
 import { Button } from '@shared/ui'
 import { useProjectStatusStore } from '@/store/status'
-import { useMemberStore } from '@/store/member'
-import MemberAvatar from '@/components/MemberAvatar'
-import './column.css'
-
-interface ICol {
-  name: string
-  data: number
-}
-
-interface IColumns {
-  xaxis: string
-  data: ICol[]
-}
-
-interface ISerieItem {
-  name: string
-  id?: string
-  data: number[]
-}
+import ChartColumn, { ISerieItem } from '@/components/ChartColumn'
 
 interface IChartColumnOption {
   series: ISerieItem[]
@@ -44,14 +22,9 @@ interface IOption {
 export default function DbCompColumn({ config, title }: IDbCompProps) {
   const [updateCounter, setUpdateCounter] = useState(0)
   const { statuses } = useProjectStatusStore()
-  const { members } = useMemberStore()
   const [data, setData] = useState({
     loading: true,
-    title: title,
-    icon: config.icon as string,
-    summary: 0,
-    percent: 0,
-    color: ''
+    title: title
   })
 
   const [opt, setOpt] = useState<IOption>({
@@ -84,53 +57,6 @@ export default function DbCompColumn({ config, title }: IDbCompProps) {
     }
   }
 
-  let max = 0
-  const chartHeight = 300
-  const heightPerRow = 30
-  const yaxisLen = Math.ceil(chartHeight / heightPerRow)
-  const xaxis = options.xaxis.categories
-
-  console.log('------------------start')
-  const columns: IColumns[] = []
-  xaxis.map((x, xid) => {
-    const series = options.series
-    const col: ICol[] = []
-    let m = 0
-    series.map(s => {
-      const val = s.data[xid]
-      m += val
-      col.push({ name: s.name, data: val })
-    })
-
-    max = m > max ? m : max
-
-    columns.push({
-      xaxis: x,
-      data: col
-    })
-  })
-
-  const yValPerCell = Math.ceil(max / yaxisLen)
-  let totalYVal = yValPerCell
-  let yaxis = new Array(yaxisLen).fill(1).map((yval, yindex) => {
-    if (yindex === 0) return totalYVal
-
-    totalYVal += yValPerCell
-
-    max = max > totalYVal ? max : totalYVal
-
-    return totalYVal
-  })
-
-  yaxis.unshift(0)
-  yaxis = yaxis.reverse()
-
-  const xAxisOffsetLeft = 60
-  const columnGap = 15
-  const columnLen = columns.length
-  const columnXOffset = Math.floor(xAxisOffsetLeft / columnLen)
-  const columnWidth = Math.floor(100 / columnLen)
-
   const convertToSerie = (columns: {
     [key: string]: { [key: string]: number }
   }) => {
@@ -138,16 +64,19 @@ export default function DbCompColumn({ config, title }: IDbCompProps) {
     const series: ISerieItem[] = []
     const validSeries = Object.keys(columns[Object.keys(columns)[0]])
 
+    // pre-define series array
     statuses.forEach(stt => {
       if (!validSeries.includes(stt.id)) return
 
       series.push({
+        color: stt.color,
         name: stt.name,
         id: stt.id,
         data: []
       })
     })
 
+    // now, fill series.data
     series.forEach(s => {
       const id = s.id
       for (const x in columns) {
@@ -180,9 +109,7 @@ export default function DbCompColumn({ config, title }: IDbCompProps) {
       })
       console.log('chartData', series, data.xAxis)
     })
-  }, [updateCounter])
-
-  console.log(yaxis)
+  }, [updateCounter, config])
 
   return (
     <div className="col-span-2 py-4 px-5 w-[] border rounded-md bg-white shadow-sm hover:shadow-lg hover:shadow-gray-200 transition-all relative overflow-hidden group cursor-pointer">
@@ -195,95 +122,8 @@ export default function DbCompColumn({ config, title }: IDbCompProps) {
           size="sm"
         />
       </h2>
-      <div>
-        <div
-          className="w-full relative my-[30px]"
-          style={{ height: chartHeight }}>
-          <div className="chart-column-yasix absolute bottom-0 left-0 h-full w-full">
-            {yaxis.map((y, yi) => {
-              return (
-                <div
-                  key={yi}
-                  style={{ height: heightPerRow }}
-                  className="text-xs flex">
-                  <div
-                    className="text-right text-gray-500 border-r pr-1 shrink-0"
-                    style={{ width: 30 }}>
-                    {y}
-                  </div>
-                  <div className="border-t w-full border-dashed"></div>
-                </div>
-              )
-            })}
-          </div>
-          <div
-            className="chart-column-items absolute bottom-0 h-full w-full flex"
-            style={{ gap: columnGap, left: xAxisOffsetLeft }}>
-            {columns.map(col => {
-              const colDatas = col.data
-              const total = colDatas.reduce((t, c) => {
-                return t + c.data
-              }, 0)
-
-              return (
-                <div
-                  key={col.xaxis}
-                  style={{
-                    width: `calc(${columnWidth}% - ${columnGap}px - ${columnXOffset}px)`
-                  }}
-                  className="h-full hover:bg-gray-100 flex flex-col-reverse relative">
-                  <div className="chart-column-name absolute -bottom-[35px] left-[50%] -translate-x-[50%] text-xs rounded">
-                    <MemberAvatar uid={col.xaxis} />
-                  </div>
-                  {colDatas.map((cd, cdidx) => {
-                    const val = cd.data
-                    const height = (val / max) * 100
-
-                    let color
-                    switch (cdidx) {
-                      case 0:
-                        color = 'blue'
-                        break
-                      case 1:
-                        color = 'green'
-                        break
-                      case 2:
-                        color = 'violet'
-                        break
-
-                      default:
-                        color = 'orange'
-                        break
-                    }
-
-                    return (
-                      <div
-                        key={cdidx}
-                        className="w-full flex items-center transition-all"
-                        style={{
-                          height: height + '%'
-                        }}>
-                        <div
-                          className="w-full rounded relative group/col-cell hover:border-2 hover:border-indigo-400"
-                          style={{
-                            height: `calc(100% - 1px)`,
-                            backgroundColor: color
-                          }}>
-                          <div className="absolute z-10 px-3 h-[25px] left-[50%] -translate-x-[50%] -top-[30px] bg-black/80 hidden rounded pointer-events-none group-hover/col-cell:flex items-center justify-center text-white text-sm">
-                            {val}
-                          </div>
-                        </div>
-                      </div>
-                    )
-                  })}
-                  <div className="text-xs text-center text-gray-400">
-                    {total}
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-        </div>
+      <div className="bg-indigo-50/20 border border-indigo-50 rounded pb-5 mt-3">
+        <ChartColumn options={options} />
       </div>
     </div>
   )
