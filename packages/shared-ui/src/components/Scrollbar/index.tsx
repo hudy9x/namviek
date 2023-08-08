34e72eebdf2a60b0878e9d381455ffe94e67d5bc
hudy9x <hudy9x@gmail.com>
2023-08-08 18:04:53 +0700

    update code

--- packages/shared-ui/src/components/Scrollbar/index.tsx
+++ packages/shared-ui/src/components/Scrollbar/index.tsx
@@ -1,3 +1,37 @@
-export default function Scrollbar() {
-  return <div></div>
+import * as ScrollArea from '@radix-ui/react-scroll-area'
+import { CSSProperties, ReactNode } from 'react'
+const TAGS = Array.from({ length: 50 }).map(
+  (_, i, a) => `v1.2.0-beta.${a.length - i}`
+)
+
+interface IScrollbarProps {
+  className?: string
+  style?: CSSProperties
+  children: ReactNode
+}
+export default function Scrollbar({
+  children,
+  className,
+  style
+}: IScrollbarProps) {
+  const classes = ['scroll-root']
+  className && classes.push(className)
+  return (
+    <ScrollArea.Root className={classes.join(' ')} style={style}>
+      <ScrollArea.Viewport className="ScrollAreaViewport">
+        {children}
+      </ScrollArea.Viewport>
+      <ScrollArea.Scrollbar
+        className="ScrollAreaScrollbar"
+        orientation="vertical">
+        <ScrollArea.Thumb className="ScrollAreaThumb" />
+      </ScrollArea.Scrollbar>
+      {/* <ScrollArea.Scrollbar */}
+      {/*   className="ScrollAreaScrollbar" */}
+      {/*   orientation="horizontal"> */}
+      {/*   <ScrollArea.Thumb className="ScrollAreaThumb" /> */}
+      {/* </ScrollArea.Scrollbar> */}
+      <ScrollArea.Corner className="ScrollAreaCorner" />
+    </ScrollArea.Root>
+  )
 }
--- packages/ui-app/app/[orgID]/project/[projectId]/board/BoardContent/BoardBodyContent/index.tsx
+++ packages/ui-app/app/[orgID]/project/[projectId]/board/BoardContent/BoardBodyContent/index.tsx
@@ -1,4 +1,5 @@
 import { useTaskStore } from 'packages/ui-app/store/task'
+import { Scrollbar } from '@shared/ui'
 import { BoardBodyTitle } from './BoardBodyTitle'
 import { BoardActionAssignee } from './BoardBodyAction/BoardActionAssignee'
 
@@ -7,17 +8,21 @@ export const BoardBodyContent = ({ statusId }: { statusId: string }) => {
 
   return (
     <>
-      {tasks.map((task, index) => {
-        if (task.taskStatusId !== statusId) return null
-        return (
-          <div
-            key={index}
-            className="bg-white mt-2 p-3 rounded-md shadow-sm h-24">
-            <BoardBodyTitle title={task.title || ''} />
-            <BoardActionAssignee taskId={task.id} uids={task.assigneeIds} />
-          </div>
-        )
-      })}
+      <Scrollbar style={{height: 600}}>
+        <div>
+          {tasks.map((task, index) => {
+            if (task.taskStatusId !== statusId) return null
+            return (
+              <div
+                key={index}
+                className="bg-white mt-2 p-3 rounded-md shadow-sm h-24">
+                <BoardBodyTitle title={task.title || ''} />
+                <BoardActionAssignee taskId={task.id} uids={task.assigneeIds} />
+              </div>
+            )
+          })}
+        </div>
+      </Scrollbar>
     </>
   )
 }
