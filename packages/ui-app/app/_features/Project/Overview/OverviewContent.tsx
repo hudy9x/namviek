6baee5171518f513aedc746165ef576b5df3c808
hudy9x <hudy9x@gmail.com>
2023-08-08 16:31:19 +0700

    add empty dashboard page

--- packages/ui-app/app/_features/Project/Overview/OverviewContent.tsx
+++ packages/ui-app/app/_features/Project/Overview/OverviewContent.tsx
@@ -1,31 +1,13 @@
-import OverviewWorkloadByStatus from './OverviewWorkloadByStatus'
-import OverviewDateRange from './OverviewDateRange'
 import './style.css'
-import OverviewMemberProgress from './OverviewMemberProgress'
-import OverviewTask from './OverviewTasks'
 import { useUser } from '@goalie/nextjs'
-import OverviewBurnoutChart from './OverviewBurnoutChart'
-import OverviewWorkloadByDate from './OverviewWorkloadByDate'
 import { Button } from '@shared/ui'
-import {
-  dboardComponentCreate,
-  dboardCreate,
-  dboardGet
-} from '@/services/dashboard'
+import { dboardCreate } from '@/services/dashboard'
 import { useParams } from 'next/navigation'
-import {
-  DashboardComponent,
-  DashboardComponentType,
-  TaskPriority
-} from '@prisma/client'
-import { useEffect, useState } from 'react'
-import DbCompSummary from '../../Dashboard/components/DbCompSummary'
-import DbComponent from '../../Dashboard/components/DbComponent'
-import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai'
 import DashboardComponentCreate from '../../Dashboard/DasboardComponentCreate'
 import { LoadingSpinner } from 'packages/shared-ui/src/components/Loading'
 import DboardComponentList from '../../Dashboard/DboardComponentList'
 import { useOverviewContext } from './context'
+import Image from 'next/image'
 
 export default function OverviewContent() {
   const { user } = useUser()
@@ -75,18 +57,34 @@ export default function OverviewContent() {
           <p className="text-gray-600 text-sm mt-2">{`Welcome back to Overview dashboard. Have a bird's eye view of your project`}</p>
         </header>
       ) : null}
-      <div className="flex items-center justify-between">
-        <div>
-          {!loading && !dboardId ? (
-            <Button
-              title="Create dashboard"
-              size="sm"
-              onClick={onCreateDashboard}
+      <div className="w-full group">
+        {!loading && !dboardId ? (
+          <div className="flex items-center gap-2 w-[720px] text-gray-500 justify-center m-auto mt-[175px] bg-white px-10 py-6 rounded-lg shadow-xl shadow-indigo-100 border-4 border-gray-200 hover:border-indigo-400 transition-all">
+            <div className="space-y-3">
+              <h2 className="text-2xl font-bold whitespace-nowrap">
+                Oops! Now dashboard found 🎃.
+              </h2>
+              <p className="pr-3 leading-7">
+                This project have no overview dashboad. Click the button below
+                to create a default dashboard
+              </p>
+              <Button
+                title="Create dashboard"
+                primary
+                onClick={onCreateDashboard}
+              />{' '}
+            </div>
+            <Image
+              src={'/business-report.png'}
+              alt="report "
+              width={250}
+              height={250}
+              quality={100}
             />
-          ) : null}
+          </div>
+        ) : null}
 
-          {dboardId ? <DashboardComponentCreate /> : null}
-        </div>
+        {dboardId ? <DashboardComponentCreate /> : null}
       </div>
       <main className="mt-3">
         <DboardComponentList />
