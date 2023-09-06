f4a84449b68d869d21be023ca5cce1f96d7827e2
quangduy0709 <92352653+quangduy0709@users.noreply.github.com>
2023-09-06 15:31:52 +0700

    fix: comments

--- packages/shared-libs/src/lib/storage.ts
+++ packages/shared-libs/src/lib/storage.ts
@@ -1,39 +1,33 @@
-import {
-  ListObjectsCommand,
-  PutObjectCommand,
-  S3Client
-} from '@aws-sdk/client-s3'
-
+import { S3 } from 'aws-sdk'
 const accessKeyId = process.env.S3_ACCESS_KEY || ''
 const secretAccessKey = process.env.S3_SECRET_KEY || ''
 const bucket = process.env.S3_BUCKET || ''
+const domain = process.env.NEXT_PUBLIC_STORAGE_DOMAIN || ''
 
-const client = new S3Client({
+const s3 = new S3({
   region: 'ap-southeast-1',
   credentials: { accessKeyId, secretAccessKey }
 })
 
 const storage = {
-  upload: async (file: File, url: string) => {
-    const { $metadata } = await client.send(
-      new PutObjectCommand({ Bucket: bucket, Key: url, Body: file })
-    )
-    return $metadata.httpStatusCode
+  upload: async (path: string, buffer: Buffer) => {
+    const data = await s3
+      .upload({ Bucket: bucket, Key: path, Body: buffer })
+      .promise()
+    // only take relative paths.
+    return data.Location.replace(domain, '')
   },
+  getFilesInFolder: async (folder: string) => {
+    let urls: string[] = []
+    s3.listObjectsV2({ Bucket: bucket, Prefix: folder }, (err, data) => {
+      if (err || data.Contents) {
+        return
+      }
 
-  getList: async (folder: string) => {
-    const data = await client.send(
-      new ListObjectsCommand({
-        Bucket: bucket,
-        Delimiter: '/',
-        Prefix: folder + '/'
-      })
-    )
-    return (
-      (data['Contents']?.map(item => `/${item['Key']}`) as string[]) ||
-      undefined
-    )
+      urls = data.Contents.map(item => `/${item.Key}`)
+    })
+
+    return urls
   }
 }
-
 export default storage
