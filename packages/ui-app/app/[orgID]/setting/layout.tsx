68fe6697fa8746e60eaffbb044c9b3ad6e7e4fe3
hudy9x <95471659+hudy9x@users.noreply.github.com>
2023-09-14 22:23:13 +0700

    feat: add member to organization directly (#49)
    this commit only add a member to organization directly via an email

--- packages/ui-app/app/[orgID]/setting/layout.tsx
+++ packages/ui-app/app/[orgID]/setting/layout.tsx
@@ -1,73 +1,13 @@
-import Link from 'next/link'
 import { ReactNode } from 'react'
-import { AiOutlineAppstoreAdd, AiOutlineCloudDownload } from 'react-icons/ai'
-import { HiOutlineInformationCircle, HiOutlineUserCircle } from 'react-icons/hi'
-import { HiOutlineServerStack } from 'react-icons/hi2'
+import SettingTabLayout from './SettingTab'
 
-export default function SettingLayout({
-  params,
-  children
-}: {
-  params: { orgID: string }
-  children: ReactNode
-}) {
-  const tabs = [
-    {
-      title: 'About',
-      name: 'about',
-      href: '#',
-      icon: HiOutlineInformationCircle,
-      current: false
-    },
-    {
-      title: 'People',
-      name: 'people',
-      href: '#',
-      icon: HiOutlineUserCircle,
-      current: false
-    },
-    {
-      title: 'Projects',
-      name: 'projects',
-      href: '#',
-      icon: HiOutlineServerStack,
-      current: false
-    },
-    {
-      title: 'Apps',
-      name: 'apps',
-      href: '#',
-      icon: AiOutlineAppstoreAdd,
-      current: false
-    },
-    {
-      title: 'Export/import',
-      name: 'export-import',
-      href: '#',
-      icon: AiOutlineCloudDownload,
-      current: false
-    }
-  ]
-
+export default function SettingLayout({ children }: { children: ReactNode }) {
   return (
     <div className="project-nav">
       <div className="bg-white border-b border-gray-200 dark:bg-gray-900 dark:border-gray-700">
         <h2 className="text-xl font-bold px-4 py-2">Settings</h2>
-        <div className="tab pl-1">
-          {tabs.map((tab, index) => {
-            const Icon = tab.icon
-            // const active = tab.name.toLowerCase() === mode
-            return (
-              <Link
-                href={`${params.orgID}/setting/${tab.name}`}
-                className={`tab-item`}
-                key={index}>
-                <Icon />
-                <span>{tab.title}</span>
-              </Link>
-            )
-          })}
-        </div>
+
+        <SettingTabLayout />
       </div>
 
       <div
---
+++ packages/ui-app/app/[orgID]/setting/people/page.tsx
@@ -0,0 +1,10 @@
+import SettingPeopleContent from '@/features/SettingPeople'
+
+export default function SettingPeople() {
+  return (
+    <div>
+      <title>{'Settings > People'}</title>
+      <SettingPeopleContent />
+    </div>
+  )
+}
