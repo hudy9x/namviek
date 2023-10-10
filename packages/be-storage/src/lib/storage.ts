7e286a699cbf56e6ea9523b613e38669e1163d21
hudy9x <hudy9x@gmail.com>
2023-10-10 11:20:17 +0700

    chore: remove aws key logging

--- packages/be-storage/src/lib/storage.ts
+++ packages/be-storage/src/lib/storage.ts
@@ -15,12 +15,6 @@ import { randomUUID } from 'crypto'
 const AWS_REGION = process.env.AWS_REGION
 const AWS_S3_BUCKET = process.env.AWS_S3_BUCKET
 
-console.log('AWS keys')
-console.log(process.env.AWS_REGION)
-console.log(process.env.AWS_S3_BUCKET)
-console.log(process.env.AWS_ACCESS_KEY)
-console.log(process.env.AWS_SECRET_ACCESS_KEY)
-
 const s3Client = new S3Client({
   region: AWS_REGION,
   credentials: {
