a21bc26cf1b9eab9044b230bb6e5c64da2c418e6
hudy9x <hudy9x@gmail.com>
2023-10-12 11:28:18 +0700

    refactor: active the favorite item that matches url

--- packages/ui-app/app/_features/Favorites/FavoriteItem.tsx
+++ packages/ui-app/app/_features/Favorites/FavoriteItem.tsx
@@ -2,12 +2,12 @@ import { Favorites } from '@prisma/client'
 import FavoriteProjectItem from './FavoriteProjectItem'
 import FavoritePageItem from './FavoritePageItem'
 
-export default function FavoriteItem({ data }: { data: Favorites }) {
+export default function FavoriteItem({ data, active }: { data: Favorites, active: boolean }) {
   const { type } = data
 
-  if (type === 'PROJECT') return <FavoriteProjectItem data={data} />
+  if (type === 'PROJECT') return <FavoriteProjectItem data={data} active={active} />
 
-  if (type === 'PAGE') return <FavoritePageItem data={data} />
+  if (type === 'PAGE') return <FavoritePageItem data={data} active={active} />
 
   return null
 }
