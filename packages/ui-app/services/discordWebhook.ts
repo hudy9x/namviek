e5bab1f5cca5f03543ffc4ce8de1ad047fb66042
vanbeonhv <nguyenvannhv26@gmail.com>
2023-10-26 18:32:29 +0700

    feat(discord notification):  Add api function for fe

--- packages/ui-app/services/discordWebhook.ts
+++ packages/ui-app/services/discordWebhook.ts
@@ -1,6 +1,39 @@
 import { DiscordWebhook } from '@prisma/client'
 import { httpPost } from './_req'
 
+export interface IDiscordWebhookQuery {
+  discordWebhookId?: string
+  projectId?: string
+}
+
 export const discordWebhookAdd = (data: Omit<DiscordWebhook, 'id'>) => {
+  return httpPost('/api/project/discord-webhooks', data)
+}
+
+export const discordWebhookUpdate = (data: Omit<DiscordWebhook, 'id'>) => {
+  return httpPost('/api/project/discord-webhooks', data)
+}
+
+export const discordWebhookGetOne = (
+  query: IDiscordWebhookQuery,
+  abortSignal?: AbortSignal
+) => {
+  return httpPost('/api/project/discord-webhooks/query', {
+    params: query,
+    signal: abortSignal
+  })
+}
+
+export const discordWebhookGetMany = (
+  query: string,
+  abortSignal?: AbortSignal
+) => {
+  return httpPost(`/api/project/discord-webhooks`, {
+    params: query,
+    signal: abortSignal
+  })
+}
+
+export const discordWebhookDelete = (data: Omit<DiscordWebhook, 'id'>) => {
   return httpPost('/api/project/discord-webhooks', data)
 }
