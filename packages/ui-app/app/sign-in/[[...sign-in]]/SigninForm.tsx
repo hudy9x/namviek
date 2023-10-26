a162bf650bde98e4ba0daca0cb169fbc5d6ef635
hudy9x <hudy9x@gmail.com>
2023-10-26 13:47:17 +0700

    refactor: update verification email style

--- packages/ui-app/app/sign-in/[[...sign-in]]/SigninForm.tsx
+++ packages/ui-app/app/sign-in/[[...sign-in]]/SigninForm.tsx
@@ -135,25 +135,22 @@ export default function SigninForm() {
             <div
               onClick={() => setIsUserInactive(false)}
               className="absolute top-0 left-0 w-full h-full bg-white/50 dark:bg-gray-900/50 backdrop-blur-md"></div>
-            <div className="relative z-10 w-[400px] rounded-md py-8 px-8 bg-white dark:bg-gray-900 border-2 dark:border-indigo-500 mt-6 text-sm text-center text-gray-400">
-              <BsMailbox className="w-14 h-14 rounded-md py-2 px-3 bg-indigo-500/60 text-white inline-block" />
-              <h2 className="text-2xl font-bold mt-4 text-gray-200">
+            <div className="relative z-10 w-[400px] rounded-md py-8 px-8 bg-white dark:bg-gray-900 border-2 border-indigo-500 mt-6 text-sm text-center text-gray-400">
+              <BsMailbox className="w-14 h-14 rounded-md py-2 px-3 bg-indigo-500/80 dark:bg-indigo-500/60 text-white inline-block" />
+              <h2 className="text-2xl font-bold mt-4 text-gray-600 dark:text-gray-200">
                 Verify your email !
               </h2>
               <p className="mt-4">
                 We found that you are already signed up your account. But have
                 not verify it via email.
               </p>
-              <p>
-                Click on the below button to resend a verification link to your
-                email
-              </p>
+              <p>Click on the below button to resend a verification email</p>
               <Button
                 className="mt-4"
                 loading={sending}
                 primary
                 block
-                title="Resend verification link"
+                title="Resend"
                 onClick={onResend}
               />
             </div>
