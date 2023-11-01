3ac499d5b561c972e135ecc73361b85fe191a74c
hudy9x <hudy9x@gmail.com>
2023-11-01 14:57:19 +0700

    feat(shared-ui): create Tooltip component

--- packages/shared-ui/src/index.ts
+++ packages/shared-ui/src/index.ts
@@ -1,29 +1,36 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
 
-import ModalComp from './components/Modal';
-import * as FormControls from './components/Controls';
-import ButtonControl from './components/Button';
-import { LoadingSpinner } from './components/Loading';
-import useFormHook from './hooks/useForm';
-import AvatarContainer from './components/Avatar';
-import DatePickerContainer from './components/DatePicker';
-import BorderlessDatePickerContainer from "./components/DatePicker/Borderless";
-import ScrollbarContainer from "./components/Scrollbar";
-import FormGroupContainer from "./components/FormGroup";
+import ModalComp from './components/Modal'
+import * as FormControls from './components/Controls'
+import ButtonControl from './components/Button'
+import { LoadingSpinner } from './components/Loading'
+import useFormHook from './hooks/useForm'
+import AvatarContainer from './components/Avatar'
+import DatePickerContainer from './components/DatePicker'
+import BorderlessDatePickerContainer from './components/DatePicker/Borderless'
+import ScrollbarContainer from './components/Scrollbar'
+import FormGroupContainer from './components/FormGroup'
+import TooltipContainer from './components/Tooltip'
 
-export { messageInfo, messageError, messageSuccess, messageWarning } from './components/Message';
+export {
+  messageInfo,
+  messageError,
+  messageSuccess,
+  messageWarning
+} from './components/Message'
 
-export const Modal = ModalComp;
-export const Form = FormControls;
-export const Button = ButtonControl;
-export const useForm = useFormHook;
-export const Loading = LoadingSpinner;
-export const Avatar = AvatarContainer;
-export const DatePicker = DatePickerContainer;
-export const DatePickerBorderless = BorderlessDatePickerContainer;
+export const Modal = ModalComp
+export const Form = FormControls
+export const Button = ButtonControl
+export const useForm = useFormHook
+export const Loading = LoadingSpinner
+export const Avatar = AvatarContainer
+export const DatePicker = DatePickerContainer
+export const DatePickerBorderless = BorderlessDatePickerContainer
 export const Scrollbar = ScrollbarContainer
 export const FormGroup = FormGroupContainer
+export const Tooltip = TooltipContainer
 export * from './components/Confirmbox'
 
-export * from './components/Controls/ListControl/type';
+export * from './components/Controls/ListControl/type'
 export * from './components/utils'
