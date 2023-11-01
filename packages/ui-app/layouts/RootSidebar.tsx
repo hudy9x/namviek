9c143ae484c029723f1dbb019a2354e3b25848fc
hudy9x <hudy9x@gmail.com>
2023-11-01 15:17:56 +0700

    refactor: update root menu icon and link

--- packages/ui-app/layouts/RootSidebar.tsx
+++ packages/ui-app/layouts/RootSidebar.tsx
@@ -7,7 +7,8 @@ import { useParams, useRouter } from 'next/navigation'
 import Image from 'next/image'
 import { HiOutlineUserPlus } from 'react-icons/hi2'
 import { LiaCitySolid } from 'react-icons/lia'
-import { FcOrganization, FcVideoCall } from 'react-icons/fc'
+import { FcBriefcase, FcOrganization, FcVideoCall } from 'react-icons/fc'
+import { Tooltip } from '@shared/ui'
 
 export default function RootSidebar() {
   const { orgID } = useParams()
@@ -25,19 +26,29 @@ export default function RootSidebar() {
             />
           </div>
         </Link>
-        <Link href={`/${orgID}/my-works`}>
-          <RiDashboardFill className="main-nav-icon active" />
-        </Link>
+
         <Link href="/organization">
-          <FcOrganization className="main-nav-icon" />
-          {/* <LiaCitySolid className="main-nav-icon" /> */}
+          <Tooltip title="Organization" side="right">
+            <div>
+              <FcOrganization className="main-nav-icon" />
+            </div>
+          </Tooltip>
         </Link>
+
+        <Link href={`/${orgID}/my-works`}>
+          <Tooltip title="Project" side="right">
+            <div>
+              <FcBriefcase className="main-nav-icon active" />
+            </div>
+          </Tooltip>
+        </Link>
+
         <Link href={`/${orgID}/meeting`}>
-          <FcVideoCall className="main-nav-icon" />
-          {/* <RiBarChartFill className="main-nav-icon" /> */}
-        </Link>
-        <Link href="/home">
-          <IoIosPlayCircle className="main-nav-icon" />
+          <Tooltip title="Meeting" side="right">
+            <div>
+              <FcVideoCall className="main-nav-icon" />
+            </div>
+          </Tooltip>
         </Link>
       </section>
       <section>
