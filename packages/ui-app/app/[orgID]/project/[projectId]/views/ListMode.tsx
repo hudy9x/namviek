d3c6946005cc6f0d032105cea87c216221abb13e
hudy9x <hudy9x@gmail.com>
2023-11-02 14:59:32 +0700

    refactor: redirect user to sign-in page when session expired

--- packages/ui-app/app/[orgID]/project/[projectId]/views/ListMode.tsx
+++ packages/ui-app/app/[orgID]/project/[projectId]/views/ListMode.tsx
@@ -40,7 +40,7 @@ export default function ListMode() {
           <div
             className="bg-white dark:bg-gray-900 mb-4 rounded-md border dark:border-gray-800 mx-4 relative mt-4"
             key={group.id}>
-            <div className="px-3 py-2 border-b dark:border-b-gray-800 sticky top-[45px] bg-white dark:bg-gray-900 rounded-t-md flex items-center justify-between z-10">
+            <div className="px-3 py-2 border-b dark:border-b-gray-800 sticky top-[40px] bg-white dark:bg-gray-900 rounded-t-md flex items-center justify-between z-10">
               <div
                 style={{ color: group.color }}
                 className="flex gap-2 items-center text-xs uppercase font-bold">
