9640e3cf602739151d6ad1cd7f7bf16f8f9aad81
hudy9x <hudy9x@gmail.com>
2023-12-08 15:44:37 +0700

    update code

--- packages/scheduler/src/main.ts
+++ packages/scheduler/src/main.ts
@@ -1,12 +1,11 @@
 import cron from 'node-cron'
 import { reqApi } from './req'
 
-console.log('start cronjob')
-cron.schedule('*/2 * * * * *', () => {
-  // cron.schedule('45 8 * * *', () => {
+// cron.schedule('*/2 * * * * *', () => {
+cron.schedule('45 8 * * *', () => {
   try {
     console.log('cron run')
-    // reqApi.get('/scheduler/remind-at-0845am')
+    reqApi.get('/scheduler/remind-at-0845am')
   } catch (error) {
     console.log(error.message)
   }
