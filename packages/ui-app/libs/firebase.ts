f7f7464f27f33808e15b443f65dc2f5fedb0d8eb
hudy9x <hudy9x@gmail.com>
2024-01-03 08:35:32 +0700

    add firebase

---
+++ packages/ui-app/libs/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
