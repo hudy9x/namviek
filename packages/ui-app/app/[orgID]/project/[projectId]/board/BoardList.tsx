5ccb78f96b1166fc4975d95c58f7ddff33540efa
hudy9x <hudy9x@gmail.com>
2024-01-04 21:34:59 +0700

    update code

--- packages/ui-app/app/[orgID]/project/[projectId]/board/BoardList.tsx
+++ packages/ui-app/app/[orgID]/project/[projectId]/board/BoardList.tsx
@@ -2,6 +2,8 @@ import { Droppable } from 'react-beautiful-dnd'
 
 import BoardItemDraggable from './BoardItemDraggable'
 import { BoardActionCreateTask } from './BoardActionCreateTask'
+import { useTaskStore } from '@/store/task'
+import { Loading } from '@shared/ui'
 
 export default function BoardList({
   items,
@@ -10,6 +12,7 @@ export default function BoardList({
   items: string[]
   groupId: string
 }) {
+  const { taskLoading } = useTaskStore()
   return (
     <Droppable droppableId={groupId} type="task">
       {provided => (
@@ -17,6 +20,10 @@ export default function BoardList({
           className="board-list"
           ref={provided.innerRef}
           {...provided.droppableProps}>
+
+
+          <Loading className='px-3 py-3' title='Loading ...' enabled={taskLoading} />
+
           {items.map((item, itemIndex) => {
             return (
               <BoardItemDraggable item={item} key={item} index={itemIndex} />
