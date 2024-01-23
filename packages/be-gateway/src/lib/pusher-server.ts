2dd599386c407373602016473da155a891308ed3
hudy9x <hudy9x@gmail.com>
2024-01-23 16:56:50 +0700

    fix: enable TLS config for pusher

--- packages/be-gateway/src/lib/pusher-server.ts
+++ packages/be-gateway/src/lib/pusher-server.ts
@@ -4,5 +4,6 @@ export const pusherServer = new Pusher({
   appId: process.env.PUSHER_CHANNEL_APP_ID,
   key: process.env.NEXT_PUBLIC_PUSHER_CHANNEL_APP_KEY,
   secret: process.env.PUSHER_CHANNEL_SECRET,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CHANNEL_APP_CLUSTER
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CHANNEL_APP_CLUSTER,
+  useTLS: true
 })
