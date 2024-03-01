23e2f305d85c41e9512f0dcdaef1078520da41b1
hudy9x <hudy9x@gmail.com>
2024-03-01 10:07:08 +0700

    fix: slow down the click action to 200ms

--- packages/ui-app/app/_features/TaskImport/index.tsx
+++ packages/ui-app/app/_features/TaskImport/index.tsx
@@ -52,7 +52,7 @@ export default function TaskImport() {
             />
             <DropdownMenu.Content>
               <DropdownMenu.Item
-                onClick={() => setVisible(true)}
+                onClick={() => setTimeout(() => setVisible(true), 200)}
                 title={
                   <div className="flex items-center gap-2">
                     <PiMicrosoftExcelLogo className="w-5 h-5 text-gray-600" />
