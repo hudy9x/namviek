63831a1c25ec9bd73c34ba547e28be06f751ea14
hudy9x <hudy9x@gmail.com>
2024-03-11 14:38:16 +0700

    update code

--- packages/be-gateway/src/lib/log.ts
+++ packages/be-gateway/src/lib/log.ts
@@ -1,3 +1,30 @@
+import { createLogger, format, transports } from 'winston'
+
+const { combine, timestamp, label, prettyPrint, simple } = format
+
+// const logger = createLogger({
+//   format: combine(label({ label: '' }), timestamp(), prettyPrint()),
+//   transports: [
+//     new transports.Console({ format: simple() }),
+//     new transports.File({ filename: '/home/bitnami/logs/backend.log' })
+//   ]
+// })
+
+// export const logging = logger
+
+export const createModuleLog = (module: string) => {
+  return createLogger({
+    format: combine(label({ label: module }), timestamp(), prettyPrint()),
+    transports: [
+      new transports.Console({ format: simple() }),
+      new transports.File({
+        format: simple(),
+        filename: 'logs/backend.log'
+      })
+    ]
+  })
+}
+
 // import { Logtail } from '@logtail/node'
 //
 // const logtail = new Logtail(process.env.LOGTAIL_SOURCE_TOKEN)
