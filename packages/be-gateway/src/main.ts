730594a6bcd2b45d2a507485fe06461bccae44e0
hudy@mac-air <hudy9x@gmail.com>
2024-06-26 17:29:28 +0700

    refactor: change render service name and add default html page for / route

--- packages/be-gateway/src/main.ts
+++ packages/be-gateway/src/main.ts
@@ -25,6 +25,28 @@ Running in ${isDevMode() ? "Development" : "Production"} mode
 ------------------------------
 `)
 
+app.get('/', (req, res) => {
+  res.send(`
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+  <div class="h-screen w-screen bg-black flex items-center justify-center">
+    <div style="width: 800px" class="text-gray-100 text-center">
+      <h1 class="text-5xl font-bold">The Backend is available !</h1>
+      <p class="mt-8">Last deployment time</p>
+      <p class="mt-2">${new Date()}</p>
+    </div>
+  </div>
+</body>
+</html>
+`)
+})
+
 app.get('/check-health', (req, res) => {
   // Log.info('Heelo')
   // Log.flush()
