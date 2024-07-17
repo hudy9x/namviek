5ba8ab93504423f9b80154ab2c9a64247797747f
hudy9x <hudy9x@gmail.com>
2024-07-17 08:42:20 +0700

    update code

--- packages/ui-app/public/sw-cache-pages.js
+++ packages/ui-app/public/sw-cache-pages.js
@@ -1,18 +1,68 @@
-const cacheName = 'v1'
+// everytime you deploy new frontend version, please update the cache version
+const cacheVersion = 'v1.1'
 
 const cacheClone = async (e) => {
   const res = await fetch(e.request);
   const resClone = res.clone();
 
-  const cache = await caches.open(cacheName);
+  const cache = await caches.open(cacheVersion);
   await cache.put(e.request, resClone);
   return res;
 };
 
+const deleteOldCaches = async () => {
+  const keys = await caches.keys()
+  keys.map(async k => {
+    if (k === cacheVersion) return
+    console.log('delete key', k)
+    await caches.delete(k)
+  })
+}
+
+
+const isEmojiResources = (url) => {
+  return url.includes('cdn.jsdelivr.net/npm/emoji-datasource-twitter/img')
+}
+
+const cacheEmojiResources = async (e) => {
+  const cache = await caches.open(cacheVersion);
+
+  const res = await fetch(e.request);
+  const resClone = res.clone();
+
+  await cache.put(e.request, resClone);
+  return res;
+}
+
+const cachedNextStaticResources = async () => {
+  console.log(1)
+}
+
 const fetchEvent = () => {
+
+  // delete old caches
+  deleteOldCaches()
+
+
+  // return default request 
+
   self.addEventListener('fetch', (e) => {
-    console.log('----------------------')
-    console.log('e.request:', e.request.method, e.request.url)
+    // console.log('----------------------')
+    // console.log('e.request:', e.request.method, e.request.url)
+
+    // cache fixed images like emoji picker
+
+    const url = e.request.url
+    if (isEmojiResources(url)) {
+      console.log(url)
+      cacheEmojiResources(e)
+      return
+    }
+
+    // cache _next/static
+    cachedNextStaticResources()
+
+    e.respondWith(fetch(e.request))
     // e.respondWith(
     //   cacheClone(e)
     //     .catch(() => caches.match(e.request))
