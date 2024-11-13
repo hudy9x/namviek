21aa37bb5b05904829fb382ae5ce3ff417d312a6
hudy@mac-air <hudy9x@gmail.com>
2024-11-13 10:34:43 +0700

    remove comment code

--- packages/be-gateway/src/lib/utils.ts
+++ packages/be-gateway/src/lib/utils.ts
@@ -4,7 +4,6 @@ export const isDevMode = () => {
 
 export const isEmailVerificationEnabled = () => {
   const isEnabled = process.env.EMAIL_VERIFICATION_ENABLED
-  console.log('isEnabled', isEnabled)
   return isEnabled === '1' || isEnabled === 'true'
 }
 
