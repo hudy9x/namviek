64f20e9e54d87a19bf195bee8d49adb13d780c3f
hudy9x <95471659+hudy9x@users.noreply.github.com>
2024-11-13 10:38:58 +0700

    refactor: allow to enable email verification (#254)

--- packages/be-gateway/src/lib/utils.ts
+++ packages/be-gateway/src/lib/utils.ts
@@ -2,6 +2,11 @@ export const isDevMode = () => {
   return process.env.DEV_MODE
 }
 
+export const isEmailVerificationEnabled = () => {
+  const isEnabled = process.env.EMAIL_VERIFICATION_ENABLED
+  return isEnabled === '1' || isEnabled === 'true'
+}
+
 export const isProdMode = () => {
   return !isDevMode()
 }
