8f76d38ec8ab59e5d613031bfb623cde8ac55cc9
hudy9x <hudy9x@gmail.com>
2024-11-25 21:00:19 +0700

    clear checked boxes when projectId and mode changes

--- packages/ui-app/app/_features/CustomFieldCheckbox/ClearCheckedCheckboxes.tsx
+++ packages/ui-app/app/_features/CustomFieldCheckbox/ClearCheckedCheckboxes.tsx
@@ -1,14 +1,16 @@
-import { useParams } from "next/navigation"
+import { useParams, useSearchParams } from "next/navigation"
 import { useEffect } from "react"
 import { useCheckboxStore } from "./useCheckboxStore"
 
 export default function ClearCheckedCheckboxes() {
   const { projectId } = useParams()
+  const sp = useSearchParams()
+  const mode = sp.get('mode')
   const clear = useCheckboxStore(state => state.clear)
 
   useEffect(() => {
     clear()
-  }, [projectId])
+  }, [projectId, mode, clear])
 
   return null
 }
