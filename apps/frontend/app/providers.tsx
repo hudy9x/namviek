34da00bf6d6e160b4ad0700d278f34011cec3664
hudy9x <95471659+hudy9x@users.noreply.github.com>
2025-01-31 10:54:57 +0700

    feat: integrate posthog for session replay (#290)

---
+++ apps/frontend/app/providers.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import posthog from 'posthog-js'
+import { PostHogProvider as PHProvider } from 'posthog-js/react'
+import { ReactNode, useEffect } from 'react'
+
+const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY || ''
+const POSTHOG_HOST = process.env.NEXT_PUBLIC_POSTHOG_HOST || ''
+
+
+export function CSPostHogProvider({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    posthog.init(POSTHOG_KEY, {
+      api_host: POSTHOG_HOST,
+      capture_pageview: false // Disable automatic pageview capture, as we capture manually
+    })
+  }, [])
+
+  return (
+    <PHProvider client={posthog}>
+      {children}
+    </PHProvider>
+  )
+}
+
