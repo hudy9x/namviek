c0a9c095577ee361397c45fdf37d2a1abad52e4f
hudy9x <hudy9x@gmail.com>
2025-02-01 22:52:39 +0700

    refactor: disable automation tab - it's not ready to production this time

--- apps/frontend/app/_features/ProjectAdvanceTabs/index.tsx
+++ apps/frontend/app/_features/ProjectAdvanceTabs/index.tsx
@@ -13,23 +13,23 @@ export default function ProjectAdvanceTabs() {
     push(`${params.orgName}/project/${params.projectId}?mode=${name}`)
   }
 
-  const onMoveTabAdvance = (name: string, tab: string) => {
-    push(`${params.orgName}/project/${params.projectId}?mode=${name}&tab=${tab}`)
-  }
+  // const onMoveTabAdvance = (name: string, tab: string) => {
+  //   push(`${params.orgName}/project/${params.projectId}?mode=${name}&tab=${tab}`)
+  // }
 
   return <div className="project-advance-tabs flex items-center gap-2">
     <div className="tab">
-      <HasRole projectRoles={['MANAGER', 'LEADER']}>
-        <div
-          className={`tab-item ${['automation', 'automation-create'].includes(mode || '')
-            ? 'active'
-            : ''
-            }`}
-          onClick={() => onMoveTabAdvance('automation', 'rule')}>
-          <HiOutlineCpuChip />
-          <span>Automation</span>
-        </div>
-      </HasRole>
+      {/* <HasRole projectRoles={['MANAGER', 'LEADER']}> */}
+      {/*   <div */}
+      {/*     className={`tab-item ${['automation', 'automation-create'].includes(mode || '') */}
+      {/*       ? 'active' */}
+      {/*       : '' */}
+      {/*       }`} */}
+      {/*     onClick={() => onMoveTabAdvance('automation', 'rule')}> */}
+      {/*     <HiOutlineCpuChip /> */}
+      {/*     <span>Automation</span> */}
+      {/*   </div> */}
+      {/* </HasRole> */}
       <HasRole projectRoles={['MANAGER', 'LEADER', 'MEMBER']}>
         <div
           className={`tab-item ${mode === 'setting' ? 'active' : ''}`}
